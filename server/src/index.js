8489b4509de1275dc56a9d621f18520e806b9286
Franco <francocorniglione5@gmail.com>
2023-07-05 14:55:01 -0300

    small changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const { authRouter, activitiesRouter, usersRouter } = require("./routes");
 const swaggerDocs = require("./routes/swagger");
 const connectAndPopulateDb = require("./database");
 require("dotenv").config();
 
-const routerUsers = require("./routes/users.js");
-const { authRouter, activitiesRouter } = require("./routes");
-
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors());
 
 // routes
-app.use('/auth', authRouter);
+app.use("/auth", authRouter);
 app.use("/activities", activitiesRouter);
-app.use("/users", routerUsers);
+app.use("/users", usersRouter);
 
 const PORT = process.env.PORT || 5000;
 
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,4 +1,5 @@
 const authRouter = require("./auth");
 const activitiesRouter = require("./activities");
+const usersRouter = require("./users");
 
-module.exports = { authRouter, activitiesRouter };
+module.exports = { authRouter, activitiesRouter, usersRouter };
