eb58670f25efd8b37ca7a356f017757933309396
migmm <micmigm@gmail.com>
2023-07-05 14:24:34 -0300

    Users crud

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const connectAndPopulateDb = require("./database");
 require("dotenv").config();
+const routerUsers = require("./routes/users.js");
 
 //routes
 const {
@@ -12,10 +13,12 @@ const {
 const app = express(); 
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(morgan("dev"));
 app.use(cors());
 
 app.use('/auth', authRouter);
+app.use("/users", routerUsers);
 
 const PORT = process.env.PORT || 5000;
 
