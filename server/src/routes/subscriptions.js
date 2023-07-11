e413d5ae75879df31b0802b1a706403ee7be61b5
Franco <francocorniglione5@gmail.com>
2023-07-10 21:11:23 -0300

    solving import error

--- server/src/routes/subscriptions.js
+++ server/src/routes/subscriptions.js
@@ -2,32 +2,35 @@ const { Router } = require("express");
 const { addUserNewSubscription, deleteUserSubscription } = require("../controllers/subscription");
 
 const { validateJWT } = require("../middlewares/validate-jwt");
-const hasRole = require("../middlewares/validate-rol");
+const hasRole = require("../middlewares/validate-role");
 const { validateFields } = require("../middlewares/validate-fields");
 const { body, param } = require("express-validator");
 const { idIsNotAdmin, idIsNotAffiliate } = require("../helpers/db-validators");
 
 const router = Router();
 
-router.put("/:id/editSubscription", 
-[
+router.put(
+  "/:id/editSubscription",
+  [
     validateJWT,
     hasRole(["admin"]),
     param("id", "id is not a MongoId").isMongoId(),
     param("id").custom(idIsNotAffiliate),
     validateFields
-],
-addUserNewSubscription);
+  ],
+  addUserNewSubscription
+);
 
-router.delete("/:id/editSubscription", 
-[
+router.delete(
+  "/:id/editSubscription",
+  [
     validateJWT,
     hasRole(["admin"]),
     param("id", "id is not a MongoId").isMongoId(),
     param("id").custom(idIsNotAffiliate),
     validateFields
-],
-deleteUserSubscription);
-
+  ],
+  deleteUserSubscription
+);
 
 module.exports = router;
