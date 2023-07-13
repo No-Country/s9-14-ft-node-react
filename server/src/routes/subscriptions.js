d05f141a654c8a1d9caa6f4e0ec5289c9623ca49
MirpoCasas <mirpocasas@gmail.com>
2023-07-13 14:12:35 -0300

    basic login

--- server/src/routes/subscriptions.js
+++ server/src/routes/subscriptions.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { addUserNewSubscription, deleteUserSubscription } = require("../controllers/subscription");
 
 const { validateJWT } = require("../middlewares/validate-jwt");
-const hasRole = require("../middlewares/validate-rol");
+const hasRole = require("../middlewares/validate-role");
 const { validateFields } = require("../middlewares/validate-fields");
 const { body, param } = require("express-validator");
 const { idIsNotAdmin, idIsNotAffiliate } = require("../helpers/db-validators");
