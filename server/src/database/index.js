a2667a7aa123a3c0e14e98d6b0659413c0ea4d15
Francisco Rey <reyf1998@outlook.es>
2023-07-16 21:01:14 -0300

    pull dev

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -12,7 +12,7 @@ const connectAndPopulateDb = async () => {
   // connect to the db
   try {
     await mongoose
-      .connect(CLUSTER_URI)
+      .connect(MONGO_URI)
       .then(() => console.log("Database connected successfully!"))
       .catch(error => console.error(`Database connection error: ${error}`));
 
