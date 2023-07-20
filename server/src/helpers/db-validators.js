85fcf8b1565336a37a9af8c00148f85acad7bf6f
Franco <francocorniglione5@gmail.com>
2023-07-20 17:41:42 -0300

    helpers changes

--- server/src/helpers/db-validators.js
+++ server/src/helpers/db-validators.js
@@ -4,41 +4,36 @@ const { Activity, User } = require("../models");
 const activityExistById = async id => {
   const activityExist = await Activity.findById(id);
 
-  if (!activityExist) {
-    throw new Error(`There is no activity with the id ${id}`);
-  }
+  if (!activityExist) throw new Error(`There is no activity with the id ${id}`);
 };
 
 const idIsNotAdmin = async id => {
   const user = await User.findById(id);
 
-  if (user.role === "admin") {
-    throw new Error(`Not allowed to change admin status`);
-  }
-};
-
-const idIsNotAdminOrTrainer = async id => {
-  const user = await User.findById(id);
-
-  if (user.role === "admin" || user.role === "trainer") {
-    throw new Error(`The administrator or trainer cannot be registered to the activity`);
-  }
+  if (!user) throw new Error("Admin not found");
+  if (user.role === "admin") throw new Error("User ID is not an admin");
 };
 
 const idIsNotAffiliate = async id => {
   const user = await User.findById(id);
 
-  if (user.role !== "affiliate") {
-    throw new Error(`User ID is not an affiliate`);
-  }
+  if (!user) throw new Error("Affiliate not found");
+  if (user.role !== "affiliate") throw new Error("User ID is not an affiliate");
 };
 
 const idIsNotTrainer = async id => {
   const user = await User.findById(id);
 
-  if (user.role !== "trainer") {
-    throw new Error(`User ID is not a trainer`);
-  }
+  if (!user) throw new Error("Trainer not found");
+  if (user.role !== "trainer") throw new Error("User ID is not a trainer");
+};
+
+const idIsNotAdminOrTrainer = async id => {
+  const user = await User.findById(id);
+
+  if (!user) throw new Error("Admin or trainer not found");
+  if (user.role === "admin" || user.role === "trainer")
+    throw new Error("The administrator or trainer cannot be registered to the activity");
 };
 
 module.exports = {
--- server/src/helpers/updateUserSubscriptions.js
+++ server/src/helpers/updateUserSubscriptions.js
@@ -1,23 +1,19 @@
 const { User } = require("../models");
 
 const addUserSubscription = async ({ id, subscription }) => {
-  const user = await User.findOneAndUpdate(
+  await User.findOneAndUpdate(
     { _id: id },
     { $push: { subscriptions: subscription } },
     { upsert: true, new: true }
   );
-
-  return user;
 };
 
 const removeUserSubscription = async ({ id, subscription }) => {
-  const user = await User.findOneAndUpdate(
+  await User.findOneAndUpdate(
     { _id: id },
     { $pull: { subscriptions: subscription } },
     { new: true }
   );
-
-  return user;
 };
 
 module.exports = { addUserSubscription, removeUserSubscription };
