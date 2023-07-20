a7bddfa1de73938127dc58d4c5eeb9049ee0ae80
Franco <francocorniglione5@gmail.com>
2023-07-20 14:51:03 -0300

    controllers stuff

--- server/src/controllers/subscriptions.js
+++ server/src/controllers/subscriptions.js
@@ -5,33 +5,25 @@ const {
 
 const addUserNewSubscription = async (req, res) => {
   const { id } = req.params;
-  const { subscriptions } = req.body;
+  const { subscription } = req.body;
 
   try {
-    const user = await addUserSubscription({ id, subscriptions });
-
+    const user = await addUserSubscription({ id, subscription });
     res.status(200).json(user);
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      msg: "Server error"
-    });
+    return res.status(500).json({ errorMessage: error.message });
   }
 };
 
 const deleteUserSubscription = async (req, res) => {
   const { id } = req.params;
-  const { subscriptions } = req.body;
+  const { subscription } = req.body;
 
   try {
-    const user = await removeUserSubscription({ id, subscriptions });
-
+    const user = await removeUserSubscription({ id, subscription });
     res.status(200).json(user);
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      msg: "Server error"
-    });
+    return res.status(500).json({ errorMessage: error.message });
   }
 };
 
