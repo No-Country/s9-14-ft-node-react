6a6cf0d408d2e1889f8f72716cca893f022e9df2
Marco Romero <marcod.romero@gmail.com>
2023-07-25 14:36:19 -0300

    add endpoint get trainer calendar

--- server/src/routes/calendar.js
+++ server/src/routes/calendar.js
@@ -3,10 +3,11 @@ const { Router } = require("express");
 const { validateJWT } = require("../middlewares/validate-jwt");
 const hasRole = require("../middlewares/validate-role");
 const {} = require("../controllers/calendar");
-const getGymCalendar = require("../controllers/calendar");
+const { getGymCalendar, getTrainerCalendar } = require("../controllers/calendar");
 
 const router = Router();
 
 router.get("/full", [validateJWT, hasRole(["admin", "trainer", "affiliate"])], getGymCalendar);
+router.get("/trainer", [validateJWT, hasRole(["trainer"])], getTrainerCalendar);
 
 module.exports = router;
